25c7d86be5d594375f74235ae879f78abd24d740
Jbrever <jbrever7@gmail.com>
2024-09-18 17:42:37 +0530

    first Commit

---
+++ src/components/ThemeBtn.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { themeContext  } from "../context/themeContext";
+import useTheme from "../context/themeContext";
+function ThemeBtn(){
+  const {themeMode , dayMode , nightMode} = useTheme()
+  const onChangeTheme =(e)=>{
+    let check = e.target.checked
+    if(check) nightMode()
+    else dayMode()  
+  }
+  React.useEffect(()=>{
+    let card = document.querySelector('.card')
+    card.classList.remove('dark','light')
+    card.classList.add(themeMode)
+    console.log(card); 
+  },[themeMode])
+  
+  return(
+     <div className="themeBtn">
+      <input type="checkBox" onChange={onChangeTheme} />
+     </div>
+  )
+}
+export default ThemeBtn
